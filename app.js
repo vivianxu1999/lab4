3b4e1f39704588a23d93186df9098e1de6d4c277
Yiran Xu <vivianxu1999@gmail.com>
2020-01-29 18:29:19 -0800

    updated jan 29th

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-var hello = require('./routes/hello');
+var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
-
 var app = express();
 
 // all environments
@@ -37,7 +36,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/hello/:userName', hello.view);
+app.get('/project/:name', project.viewProject);
+
 // Example route
 // app.get('/users', user.list);
 
